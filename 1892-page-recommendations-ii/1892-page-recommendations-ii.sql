4d9fc5ca2889ab9b901c51cd11b6f373ce958bc8
klaajane <karame.laajane5@gmail.com>
2026-09-12 15:45:29 -0500

    Time: 768 ms (100%), Space: 0B (100%) - LeetHub

--- 1892-page-recommendations-ii/1892-page-recommendations-ii.sql
+++ 1892-page-recommendations-ii/1892-page-recommendations-ii.sql
@@ -4,23 +4,24 @@ SELECT user1_id "user_id", user2_id "friend_id" FROM friendship
     UNION
 SELECT user2_id "user_id", user1_id "friend_id" FROM friendship)
 
-SELECT
-    f.user_id, l.page_id, COUNT(DISTINCT f.friend_id) AS "friends_likes"
-FROM
-    all_friends f
-INNER JOIN
-    likes AS l
-    ON f.friend_id = l.user_id
-    
---Filter out pages that are already liked by the user
-LEFT JOIN
-    likes AS l2
-    ON f.user_id = l2.user_id
-    AND l.page_id = l2.page_id
+,
 
-WHERE l2.page_id IS NULL
-GROUP BY 1, 2
----------------------------------------------- NOTES --------------------------------------------
---> recommend a page to user_id: liked by one friend of user_id and is not liked by user_id
---> 
--------------------------------------------------------------------------------------------------
+recommended_pages AS (
+    SELECT
+        f.user_id,
+        l1.page_id,
+        COUNT(f.friend_id) AS friends_likes
+    FROM all_friends f
+    INNER JOIN likes l1
+        ON f.friend_id = l1.user_id
+    GROUP BY f.user_id, l1.page_id
+    ORDER BY f.user_id
+)
+
+SELECT *
+FROM recommended_pages r
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM likes l
+    WHERE l.user_id = r.user_id AND l.page_id = r.page_id
+)
