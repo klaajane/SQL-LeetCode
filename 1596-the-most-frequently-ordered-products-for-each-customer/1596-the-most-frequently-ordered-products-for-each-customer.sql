882f485e0dfb991f6ee9b11d44a8b4ce0597d6ec
klaajane <karame.laajane5@gmail.com>
2026-09-13 10:50:59 -0500

    Time: 636 ms (65.06%), Space: 0B (100%) - LeetHub

--- 1596-the-most-frequently-ordered-products-for-each-customer/1596-the-most-frequently-ordered-products-for-each-customer.sql
+++ 1596-the-most-frequently-ordered-products-for-each-customer/1596-the-most-frequently-ordered-products-for-each-customer.sql
@@ -1,27 +1,26 @@
-WITH orders_ranked AS (
+--> Appraoch:
+    --> 1/ find what the most frequent product (rnk = 1) purhcased (COUNT() & DENSE_RANK)
+
+    WITH orders_ranked AS (
+        SELECT
+            customer_id,
+            product_id,
+            COUNT(product_id) AS product_count,
+            DENSE_RANK() OVER (
+                PARTITION BY customer_id 
+                ORDER BY COUNT(product_id) DESC
+            ) AS rnk
+        FROM orders
+        GROUP BY customer_id, product_id
+    )
+
+    --> 2/ find out the name of the price (JOIN to products to pull the name)
+
     SELECT
         o.customer_id,
         o.product_id,
-        COUNT(o.product_id) AS order_count,
-        DENSE_RANK() OVER (
-            PARTITION BY customer_id
-            ORDER BY COUNT(o.product_id) DESC
-        ) AS rnk
-    FROM orders o
-    GROUP BY 
-        o.customer_id,
-        o.product_id
-)
-
-SELECT
-    o.customer_id,
-    o.product_id,
-    p.product_name
-FROM orders_ranked o
-INNER JOIN products p
-    ON p.product_id = o.product_id 
-WHERE rnk = 1
-
--- orders table grain is defined as one order per product per day per customer
--- should we return both products in case there's a tie?
--- 
+        p.product_name
+    FROM orders_ranked o
+    JOIN products p
+        ON p.product_id = o.product_id
+        AND o.rnk = 1
